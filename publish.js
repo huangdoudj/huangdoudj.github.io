c1287065faedebb05374eea4a8aad62b70e21a69
huangdoudj <huangdoudj@gmail.com>
2026-09-15 11:00:15 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -51,13 +51,13 @@ for(var i =0;i<urls.length*3;i++){
 }
 
 var otherUrls = [
-	'https://52dh.pages.dev',
-	'https://52dh.github.io/',
+	'https://huangdoudj.pages.dev',
+	'https://huangdoudj.github.io/',
 ];
 var foreverUrls = [
-	  'https://52crdh.net',
+	  'https://huangdougw.com',
 	JumpPage,
-	'https://52crdh.com',
+	'https://aihuangdou.com',
 	
 ];
 var notices = [
@@ -133,7 +133,7 @@ window.onload = function () {
     var newFavoritesElem =  createFieldElem({title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: ""});
     mainElem.appendChild(newFavoritesElem);
     // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问52成人导航'});
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问黄豆短剧'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
